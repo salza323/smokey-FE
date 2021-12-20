79fbea6a999e20a9270a1843b937c36ab6ce0ab6
Sal Zamora <sal.zamora480@gmail.com>
2021-12-20 08:34:21 -0700

    created button for delete and logic for sorting recipes by likes

--- smokey/src/Pages/Recipes.js
+++ smokey/src/Pages/Recipes.js
@@ -1,12 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import Button from '@mui/material/Button';
 
 import RecipePreviewCard from '../Components/RecipePreviewCard';
+import { Navigate } from 'react-router';
 
 function Recipes() {
   const [recipeData, setRecipeData] = useState([]);
+  //   const [sortingDropdown, setSortingDropdown] = useState('Recent');
+  const [displayFormat, setDisplayFormat] = useState('All');
 
-  const fetchData = () => {
+  //   const sortingDropdownOnChange = (event) => {
+  //     setSortingDropdown(event.target.value);
+
+  //     if (event.target.value === 'Recent') {
+  //       props.fetchPostCommentsByRecent(postID);
+  //     } else {
+  //       props.fetchPostCommentsByPopular(postID);
+  //     }
+  //   };
+
+  const selectDisplayFormat = (e) => {
+    setDisplayFormat(e.target.value);
+
+    if (e.target.value === 'All') {
+      getAllRecipes();
+    } else {
+      getAllRecipesByLikes();
+    }
+  };
+
+  const navigate = useNavigate();
+
+  const getAllRecipes = () => {
     axios
       .get(`http://localhost:8001/api/recipes`)
       .then(function (res) {
@@ -15,14 +42,52 @@ function Recipes() {
       .catch((err) => console.log(err.response));
   };
 
+  const getAllRecipesByLikes = () => {
+    axios
+      .get(`http://localhost:8001/api/recipes/by-likes`)
+      .then(function (res) {
+        setRecipeData(res.data.allRecipes);
+      })
+      .catch((err) => console.log(err.response));
+  };
+
   useEffect(() => {
-    fetchData();
+    getAllRecipes();
   }, []);
+
+  const goToCreateARecipe = (e) => {
+    e.preventDefault();
+    navigate('/create-recipe');
+  };
+
+  // <div className='filter'>
+  //   <label htmlFor='sort'>SORT</label>
+  //   <select
+  //     name='sort'
+  //     value={sortingDropdown}
+  //     onChange={sortingDropdownOnChange}
+  //   >
+  //     <option value='Recent'>Recent</option>
+  //     <option value='Popular'>Popular</option>
+  //   </select>
+  // </div>;
 
   return (
     <div className='recipe-data'>
       <div>
-        <p>Recipe data</p>
+        <div className='filter'>
+          <label htmlFor='sort'>SORT</label>
+          <select
+            name='sort'
+            value={displayFormat}
+            onChange={selectDisplayFormat}
+          >
+            <option value='All'>All</option>
+            <option value='Most Popular'>Most Popular</option>
+          </select>
+        </div>
+        <p>All The Recipes</p>
+        <Button onClick={goToCreateARecipe}>Post Your Own Recipe!</Button>
         {recipeData.map((singleRecipe, idx) => (
           <RecipePreviewCard key={idx} singleRecipe={singleRecipe} />
         ))}
